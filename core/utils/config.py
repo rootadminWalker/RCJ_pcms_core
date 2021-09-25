a4dd917d4aa4f22356b7dce46d9c87410f6e348e
rootadminwalker <chiioleong519@gmail.com>
2021-09-25 17:52:16 +0800

    Deprecation confirmed

--- core/utils/config.py
+++ core/utils/config.py
@@ -1,1 +1,5 @@
-ROS_RATE = 30
+from warnings import warn
+
+warn('Warning: This will be deprecated in the future, use the one in core.Nodes.Node.Node instead',
+     DeprecationWarning)
+ROS_RATE = 35
