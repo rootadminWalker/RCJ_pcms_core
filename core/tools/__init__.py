1a3244f6fdb1ce64d09910992e9133128bf8265c
rootadminwalker <chiioleong519@gmail.com>
2021-12-17 20:09:25 +0800

    In case you don't have ROS

--- core/tools/__init__.py
+++ core/tools/__init__.py
@@ -1,8 +1,13 @@
-from .Chassis import Chassis
-from .OpenPose import OpenPose
-from .PIDController import PIDController
-from .ManipulatorController import ManipulatorController
-from .Speaker import Speaker
-from .SLAMController import SLAMController
+import warnings
+try:
+    from .Chassis import Chassis
+    from .ManipulatorController import ManipulatorController
+    from .SLAMController import SLAMController
+except ImportError as e:
+    warnings.warn(f"A module was missing from these ros-specific libraries, and it won't work now\nMessage: {e}")
+
 from .SmoothAcceleration import SmoothAcceleration
 from .YourTTS import YourTTS
+from .Speaker import Speaker
+from .PIDController import PIDController
+from .OpenPose import OpenPose
