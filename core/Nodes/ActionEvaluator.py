f89596e72fe51cf2f13451083e59e829e8f2464b
rootadminwalker <chiioleong519@gmail.com>
2022-01-22 22:53:19 +0800

    :star: new(ActionEvaluator): Insufficient slot capturing: 1, Capture insufficient slot using sessions 2, Using new Node state to determine what to do 3, Added max_rounds for start_session function 4, Added documentations for for-user-to-use methods

--- core/Nodes/ActionEvaluator.py
+++ core/Nodes/ActionEvaluator.py
@@ -42,7 +42,12 @@ def dummy_callback(intent, slots, raw_text, session):
 
 class ActionEvaluator(Node):
     def __init__(self):
-        super(ActionEvaluator, self).__init__('intent_ac', anonymous=False)
+        super(ActionEvaluator, self).__init__(
+            'intent_ac',
+            anonymous=False,
+            default_state='NORMAL',
+            node_group='/intent/'
+        )
 
         # Load the intent configs
         config_file = rospy.get_param(
@@ -75,22 +80,38 @@ class ActionEvaluator(Node):
 
         self.main()
 
-    def subscribe_intent(self, intent, callback, response):
+    @staticmethod
+    def on_session() -> bool:
+        """
+        Check if there is a established session
+        Returns: True or False
+
+        """
+        return rospy.get_param('/intent_manager/on_session')
+
+    def subscribe_intent(self, intent: str, callback, response: str) -> None:
+        """
+        Assign a callback to an intent when it comes
+        Args:
+            intent: The name of the intent
+            callback: The function that handles the intent
+            response: The response of this intent
+
+        Returns:
+
+        """
         self.intent2callback[intent] = SubscribeIntent(callback, response)
 
     def insufficient_callback(self, slots, required_slot):
         self.speaker.say_until_end(required_slot.confirm_response)
         if not self.on_session():
-            self.start_session(
-                next_intents=[self.current_intent],
+            confirm_intent = required_slot.confirm_intent
+            self.__start_insufficient_session(
+                next_intents=[self.current_intent, confirm_intent],
                 custom_data=json.dumps(slots.raw_slots)
             )
         else:
-            self.continue_session(next_intents=[self.current_intent])
-
-    @staticmethod
-    def on_session():
-        return rospy.get_param('/intent_manager/on_session')
+            self.__continue_insufficient_session(next_intents=[self.current_intent])
 
     def message_cb(self, goal: IntentACControllerGoal):
         # TODO async the callback function for preempt checking
@@ -102,38 +123,41 @@ class ActionEvaluator(Node):
         if self.on_session():
             session = goal.session
 
-        # Execute the callbacks
-        if self.current_intent not in self.intent2callback:
-            rospy.logerr(f"Intent {self.current_intent}'s callback doesn't exist, doing nothing")
-            callback = dummy_callback
-        else:
-            resp_and_callback = self.intent2callback[self.current_intent]
-            self.speaker.say_until_end(resp_and_callback.response)
-            callback = resp_and_callback.callback
-
-        # Get intent configs from the config file
-        try:
-            intent_config = self.intent_configs[self.current_intent]
-        except KeyError:
-            intent_config = IntentConfigs.INTENT_DEFAULT_CONFIG
-
         # check if the slots are sufficient for executing
         slots_insufficient, required_slot = self.intent_configs.slots_insufficient(self.current_intent, slots)
         if slots_insufficient:
             self.insufficient_callback(slots, required_slot)
         else:
+            # If the last state is INSUFFICIENT and it is not NotRecognized (User said unrecognizable things),
+            # the current intent will be the started_intent of the session
+            if self.get_state() == 'INSUFFICIENT' and self.current_intent != 'NotRecognized':
+                self.current_intent = session.started_intent
+
+            # If the session has stopped already, Then nothing will happen
             try:
                 self.stop_session()
             except RuntimeError:
                 pass
+
+            # Execute the callbacks
+            if self.current_intent not in self.intent2callback:
+                rospy.logerr(f"Intent {self.current_intent}'s callback doesn't exist, doing nothing")
+                callback = dummy_callback
+            else:
+                resp_and_callback = self.intent2callback[self.current_intent]
+                self.speaker.say_until_end(resp_and_callback.response)
+                callback = resp_and_callback.callback
+
             callback(self.current_intent, slots, raw_text, session)
+
+        self.next_state_to_param()
 
         # Set the callback was executed successfully
         self.current_intent = ''
         result = IntentACControllerResult(True)
         self.action_controller_server.set_succeeded(result)
 
-    def start_session(self, next_intents, custom_data=None):
+    def __start_session_partial(self, next_intents, custom_data=None, max_rounds=1):
         if self.on_session():
             raise RuntimeError('A session has started already')
 
@@ -141,9 +165,10 @@ class ActionEvaluator(Node):
         req.session_data.started_intent = self.current_intent
         req.session_data.possible_next_intents = next_intents
         req.session_data.custom_data = json.dumps(custom_data)
+        req.session_data.max_rounds = max_rounds
         self.__start_session(req)
 
-    def continue_session(self, next_intents, custom_data=None):
+    def __continue_session_partial(self, next_intents, custom_data=None):
         if not self.on_session():
             raise RuntimeError("You can't continue a 'None' session")
 
@@ -151,11 +176,53 @@ class ActionEvaluator(Node):
         req.session_data.possible_next_intents = next_intents
         if custom_data is not None:
             req.session_data.custom_data = json.dumps(custom_data)
+        req.session_data.max_rounds += 1
         self.__continue_session(req)
 
+    def __start_insufficient_session(self, next_intents, custom_data=None, max_rounds=3):
+        self.set_state('INSUFFICIENT')
+        self.__start_session_partial(next_intents, custom_data, max_rounds)
+
+    def __continue_insufficient_session(self, next_intents, custom_data=None):
+        self.set_state('INSUFFICIENT')
+        self.__continue_session_partial(next_intents, custom_data)
+
+    def start_session(self, next_intents, custom_data=None):
+        """
+        Establish a new session, which last for one round.
+        Args:
+            next_intents: A threshold for the coming intents, can be None if you allow anybody
+            custom_data: Data to pass through the whole session
+
+        Returns:
+
+        """
+        self.set_state('ON_SESSION')
+        self.__start_session_partial(next_intents, custom_data, 1)
+
+    def continue_session(self, next_intents, custom_data=None):
+        """
+        Continue a establish session if you don't want it to perish
+        Args:
+            next_intents: A threshold for the coming intents, can be None if you allow anybody
+            custom_data: Data to pass through the whole session
+
+        Returns:
+
+        """
+        self.set_state('ON_SESSION')
+        self.__continue_session_partial(next_intents, custom_data)
+
     def stop_session(self):
+        """
+        Stop an established session
+        Returns:
+
+        """
         if not self.on_session():
             raise RuntimeError("There isn't a session started")
+
+        self.set_state('NORMAL')
         self.__stop_session()
 
     def main(self):
