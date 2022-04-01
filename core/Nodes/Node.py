632faabcaf39e2c8de9b846a738fa60ef7e3eaca
rootadminwalker <chiioleong519@gmail.com>
2022-04-01 23:53:44 +0800

    :star: new(Controller): New Controller Node type which supports state machine, core.Nodes.Node now is back to without state

--- core/Nodes/Node.py
+++ core/Nodes/Node.py
@@ -25,12 +25,13 @@ SOFTWARE.
 """
 import warnings
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Any, Callable, List
 from rich.console import Console
 
 import rospy
 
 console = Console()
+
 
 class NodeProgram:
     # TODO: Deprecate this after ActionController was merged
@@ -47,15 +48,13 @@ class NodeProgram:
 class Node(ABC):
     ROS_RATE = 35
 
-    def __init__(self, name, anonymous=False, default_state='', node_group='~'):
+    def __init__(self, name, anonymous=False):
         """
         An abstract class for initializing into a ROS node, since ROS uses callbacks and hermes protocol,
         OOP will be better for variables to cross between different borders of the program
         Args:
             name: Name of the node
             anonymous: Generate a random ID after the node name or not
-            default_state: The default state of the node, if you want a state machine
-            node_group: A group state header of multiple node
         """
         with console.status(f"[magenta] Initializing node /{name}") as status:
             self.name = name
@@ -63,10 +62,6 @@ class Node(ABC):
             console.log("Initialized ROS Node")
             self.rate = rospy.Rate(Node.ROS_RATE)
             console.log(f"Initialized ROS rate as {Node.ROS_RATE}")
-
-            self.__node_group = node_group
-            self.__state = default_state
-            rospy.set_param(f'{self.__node_group}next_state', self.__state)
 
         console.print(f"[bold green] Node /{name} initialized sucessfully")
 
@@ -109,29 +104,97 @@ class Node(ABC):
         rospy.wait_for_message(topic, data_class)
         rospy.loginfo(f'{topic}: Ok')
 
-    def set_state(self, state: Any):
+
+class Controller(Node):
+    def __init__(
+            self,
+            name: str,
+            anonymous: bool = False,
+            default_state: str = '',
+            state_param_group: str = '~',
+            states: List[str] = []
+    ):
         """
-        Set the coming next state
+        Controller node with state controlling
         Args:
-            state: Must be a base type
+            name: Name of the controller (Will be passed to rospy.init_node)
+            anonymous: Will be passed to rospy.init_node as anonymous parameter
+            default_state: The default state of the controller
+            state_param_group: The state parameter's "first name". Used when multiple controllers share states
+            states: List of states available (required)
+        """
+        super(Controller, self).__init__(name, anonymous)
+        self.default_state = default_state
+        self.state_param_group = state_param_group
+        self.states = states
+        assert len(self.states) != 0
+
+        self.state_param = f'{self.state_param_group}/state'
+
+        rospy.set_param(self.state_param, self.default_state)
+
+        # The dictionary of states and corresponding listeners
+        self.state_to_callback = {}
+
+    def add_state_listener(self, callback: Callable, state: str):
+        """
+        Add a callback listener to a state. Will be called when self.execute_states is triggered
+        Args:
+            callback: The callback that listen to the specified state
+            state: The state that correspond to the callback
 
         Returns:
 
         """
-        rospy.set_param(f'{self.__node_group}next_state', state)
-
-    def next_state_to_param(self):
-        """
-        Call this to set the cached state (next_state parameter)
-        Returns:
-
-        """
-        self.__state = rospy.get_param(f'{self.__node_group}next_state')
+        assert state in self.states
+        self.state_to_callback[state] = callback
 
     def get_state(self) -> Any:
         """
-        Get the cached state
-        Returns: A base type
+        Get the current state
+        Returns:
+            Current state
+        """
+        return rospy.get_param(self.state_param)
+
+    def set_state(self, state: Any):
+        """
+        Sets the state to new a new state
+        **WARNING** This will IMMEDIATELY replace the old state once you call it,
+                    use it wisely
+        Args:
+            state: Anything represents a state and can be taken by rospy.set_param
+
+        Returns:
 
         """
-        return self.__state
+        rospy.set_param(self.state_param, state)
+
+    def execute_states(self, *args, **kwargs):
+        """
+        This executes the listeners according to the state when this method is called
+        *args and **kwargs will be passed to the callbacks directly
+
+        Returns:
+
+        """
+        current_state = self.get_state()
+        self.state_to_callback[current_state](*args, **kwargs)
+
+    @abstractmethod
+    def main(self):
+        """
+        Your node's main loop will evaluate here
+        Returns:
+
+        """
+
+    @abstractmethod
+    def reset(self):
+        """
+        You can reset your node's variables, states, etc.
+        Default will reset the state to self.default_state
+        Returns:
+
+        """
+        self.set_state(self.default_state)
