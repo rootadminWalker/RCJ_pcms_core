73c0da1cf2f34193b4c84407a1ee961202e53af8
rootadminwalker <chiioleong519@gmail.com>
2022-04-30 00:58:03 +0800

    Hardware controllers and Utilities refactor consequences

--- core/tools/__init__.py
+++ core/tools/__init__.py
@@ -1,14 +1,1 @@
-import warnings
-try:
-    from .Chassis import Chassis
-    from .ManipulatorController import ManipulatorController
-    from .SLAMController import SLAMController
-    from .AsyncTimer import AsyncTimer
-except ImportError as e:
-    warnings.warn(f"A module was missing from these ros-specific libraries, and it won't work now\nMessage: {e}")
-
-from .SmoothAcceleration import SmoothAcceleration
-from .YourTTS import YourTTS
-from .Speaker import Speaker
-from .PIDController import PIDController
 from .OpenPose import OpenPose
