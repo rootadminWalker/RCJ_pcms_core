110cc6fa0c1025ca22d2ee1fd2c8e348df8997ca
rootadminwalker <chiioleong519@gmail.com>
2022-09-27 21:20:18 +0800

    Two packages unused by the robot

--- core/Dtypes/__init__.py
+++ core/Dtypes/__init__.py
@@ -1,9 +1,2 @@
-# from .PoseProcess import *
-from collections import namedtuple
-
-from .SnipsProcess import *
 from .boxProcess import *
 from .Namespace import Namespace
-
-# Subscribe Intent
-SubscribeIntent = namedtuple('SubscribeIntent', ['callback', 'response'])
